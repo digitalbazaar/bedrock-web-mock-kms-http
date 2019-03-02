97d2f586ad1ed20da94f5416dd0d3a1674ed42b4
Dave Longley <dlongley@digitalbazaar.com>
2019-03-02 15:50:40 -0500

    Remove unnecessary indent.

--- MockKmsService.js
+++ MockKmsService.js
@@ -18,62 +18,60 @@ export class MockKmsService {
       operations: `${root}/operations`
     };
 
-    server.map(() => {
-      server.post(routes.operations, async request => {
-        // lowercase headers
-        const headers = {};
-        for(const key in request.requestHeaders) {
-          headers[key.toLowerCase()] = request.requestHeaders[key];
+    server.post(routes.operations, async request => {
+      // lowercase headers
+      const headers = {};
+      for(const key in request.requestHeaders) {
+        headers[key.toLowerCase()] = request.requestHeaders[key];
+      }
+      const requestOptions = {
+        method: request.method,
+        url: request.url,
+        headers
+      };
+
+      // get `controller` from key ID in Authorization header
+      const parsed = parseRequest(
+        requestOptions, {headers: ['expires', 'host', '(request-target)']});
+      const controller = parsed.keyId;
+
+      // parse operation from POST data
+      const operation = JSON.parse(request.requestBody);
+
+      const {method, parameters, plugin} = operation;
+      // TODO: validate method, parameters, plugin
+
+      // prevent calling private methods
+      if(typeof method !== 'string' || method.startsWith('_')) {
+        return [
+          400,
+          {json: true},
+          new TypeError('"method" must be a string.')
+        ];
+      }
+
+      // ensure plugin exists and supports operation method
+      const pluginApi = this.plugins.get(plugin);
+      if(!(pluginApi && typeof pluginApi[method] === 'function')) {
+        return [
+          400,
+          {json: true},
+          new TypeError(`Method "${method}" is not supported.`)
+        ];
+      }
+
+      let result;
+      try {
+        result = await pluginApi[method]({...parameters, controller});
+      } catch(e) {
+        let code = 500;
+        if(e instanceof TypeError) {
+          code = 400;
         }
-        const requestOptions = {
-          method: request.method,
-          url: request.url,
-          headers
-        };
+        return [code, {json: true}, e];
+      }
 
-        // get `controller` from key ID in Authorization header
-        const parsed = parseRequest(
-          requestOptions, {headers: ['expires', 'host', '(request-target)']});
-        const controller = parsed.keyId;
-
-        // parse operation from POST data
-        const operation = JSON.parse(request.requestBody);
-
-        const {method, parameters, plugin} = operation;
-        // TODO: validate method, parameters, plugin
-
-        // prevent calling private methods
-        if(typeof method !== 'string' || method.startsWith('_')) {
-          return [
-            400,
-            {json: true},
-            new TypeError('"method" must be a string.')
-          ];
-        }
-
-        // ensure plugin exists and supports operation method
-        const pluginApi = this.plugins.get(plugin);
-        if(!(pluginApi && typeof pluginApi[method] === 'function')) {
-          return [
-            400,
-            {json: true},
-            new TypeError(`Method "${method}" is not supported.`)
-          ];
-        }
-
-        let result;
-        try {
-          result = await pluginApi[method]({...parameters, controller});
-        } catch(e) {
-          let code = 500;
-          if(e instanceof TypeError) {
-            code = 400;
-          }
-          return [code, {json: true}, e];
-        }
-
-        return [200, {json: true}, result];
-      });
+      return [200, {json: true}, result];
     });
   }
 }
